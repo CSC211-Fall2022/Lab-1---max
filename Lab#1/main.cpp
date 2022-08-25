1ffce049cd1ec8bc440a9aa8f99f1d7891fb778b
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-08-25 14:07:17 -0400

    Initial Commit

---
+++ Lab#1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab#1
+//
+//  Created by Maxim Voyevoda on 8/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
