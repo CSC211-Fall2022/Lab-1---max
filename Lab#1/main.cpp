b166ff0a28b16bf6148a7f5a1fe7ad3793082dc7
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-08-29 00:43:23 -0400

    working

--- Lab#1/main.cpp
+++ Lab#1/main.cpp
@@ -1,14 +1,46 @@
-//
-//  main.cpp
-//  Lab#1
-//
-//  Created by Maxim Voyevoda on 8/25/22.
-//
-
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int operation(string input);
+
+int main() {
+
+    ifstream in("hex.txt");
+
+    if (!in){
+        cout << "File is not open";
+        exit(1);
+    }
+    
+    string val1, val2;
+    
+    getline(in, val1);
+    getline(in, val2);
+    
+    cout << operation(val1) + operation(val2) << endl;
+
+    cout << endl;
     return 0;
+}
+
+int operation(string input){
+    
+    int total = 0;
+    int num;
+    int c = 3;
+    
+    for (int i = 0; i < 4; i++){
+        //converts to corresponding number:
+        if (input[i] >= 65 && input[i] <= 70){
+            num = input[i] - 55;
+        } else {num = input[i] - 48;}
+
+        total += num * pow(16, c);
+        c--;
+    }
+
+    return total;
 }
