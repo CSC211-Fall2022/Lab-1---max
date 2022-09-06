36f09627d1fc7b5c5529ea7d2e9e8a7045d797c4
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-09-05 22:21:27 -0400

    working + extra credit

--- Lab#1/main.cpp
+++ Lab#1/main.cpp
@@ -1,181 +1,3 @@
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//#include <cmath>
-//using namespace std;
-//
-//int operation(string input);
-//
-//int main() {
-//
-//    ifstream in("hex.txt");
-//
-//    if (!in){
-//        cout << "File is not open";
-//        exit(1);
-//    }
-//
-//    string val1, val2;
-//
-//    getline(in, val1);
-//    getline(in, val2);
-//
-//    cout << operation(val1) + operation(val2) << endl;
-//
-//    cout << endl;
-//    return 0;
-//}
-//
-//int operation(string input){
-//
-//    int total = 0;
-//    int num;
-//    int c = 3;
-//
-//    for (int i = 0; i < 4; i++){
-//        //converts to corresponding number:
-//        if (input[i] >= 'A' && input[i] <= 'F'){
-//            num = input[i] - 55;
-//        } else {num = input[i] - 48;}
-//
-//        total += num * pow(16, c);
-//        c--;
-//    }
-//
-//    return total;
-//}
-
-
-//#include <iostream>
-//#include <string>
-//#include <cmath>
-//using namespace std;
-//
-//int operation(string input1, string input2);
-//
-//int main() {
-//
-//    string val1, val2;
-//
-//    getline(cin, val1);
-//    getline(cin, val2);
-//
-//
-//    if (operation(val1, val2) != 0){
-//        cout << "The decimal sum of " << val1 << " and " << val2 << " is " << operation(val1, val2) << endl;
-//    } else {
-//        cout << "CRITICAL ERROR: SELF DESTRUCT COMMENCING IN..... " << endl;
-//    }
-//
-//    cout << endl;
-//    return 0;
-//}
-//
-//int operation(string input1, string input2){
-//
-//    int total = 0;
-//    int num;
-//    int c = 3;
-//
-//    for (int i = 0; i < 4; i++){
-//        //converts to corresponding number:
-//        if (input1[i] >= 'A' && input1[i] <= 'F'){
-//            num = input1[i] - 55;
-//        }else if (input1[i] >= 'a' && input1[i] <= 'f'){
-//            num = input1[i] - 87;
-//        } else if (input1[i] >= '0' && input1[i] <= '9'){
-//            num = input1[i] - 48;
-//        } else {
-//            return 0000; //"ERROR"
-//        }
-//
-//        total += num * pow(16, c);
-//        c--;
-//    }
-//
-//    c = 3;
-//
-//    for (int i = 0; i < 4; i++){
-//        //converts to corresponding number:
-//        if (input2[i] >= 'A' && input2[i] <= 'F'){
-//            num = input2[i] - 55;
-//        }else if (input2[i] >= 'a' && input2[i] <= 'f'){
-//            num = input2[i] - 87;
-//        } else if (input2[i] >= '0' && input2[i] <= '9'){
-//            num = input2[i] - 48;
-//        } else {
-//            return 0000; //"ERROR"
-//        }
-//
-//    }
-//
-//    return total;
-//}
-
-
-
-/* WORKING!!!!!!!!!
- 
- #include <iostream>
- #include <string>
- #include <cmath>
- using namespace std;
-
- int operation(string input1, string input2);
-
- int main() {
-
-     string val1, val2;
-     
-     getline(cin, val1);
-     getline(cin, val2);
-     
-     cout << "The decimal sum of " << val1 << " and " << val2 << " is " << operation(val1, val2) << '.' << endl;
-
-     cout << endl;
-     return 0;
- }
-
- int operation(string input1, string input2){
-     
-     int total = 0;
-     int num;
-     int c = 3;
-     
-     for (int i = 0; i < 4; i++){
-         //converts to corresponding number:
-         if (input1[i] >= 'A' && input1[i] <= 'F'){
-             num = input1[i] - 55;
-         } else if (input1[i] >= 'a' && input1[i] <= 'f'){
-             num = input1[i] - 87;
-         } else {num = input1[i] - 48;}
-
-         total += num * pow(16, c);
-         c--;
-     }
-     
-     c = 3;
-     
-     for (int i = 0; i < 4; i++){
-         //converts to corresponding number:
-         if (input2[i] >= 'A' && input2[i] <= 'F'){
-             num = input2[i] - 55;
-         } else if (input2[i] >= 'a' && input2[i] <= 'f'){
-             num = input2[i] - 87;
-         } else {num = input2[i] - 48;}
-
-         total += num * pow(16, c);
-         c--;
-     }
-
-     return total;
- }
-
- 
- */
-
-
-
 #include <iostream>
 #include <cmath>
 #include <string>
@@ -195,10 +17,8 @@ int main() {
     cin.getline(val2, size);
     
     cout << "The decimal sum of " << val1 << " and " << val2 << " is " << addTwoHex(val1, val2) << '.';
-//    cout << endl << "Hex: " << decToHex(9320) << endl;
     decToHex(addTwoHex(val1, val2));
     
-   //  cout << endl;
     return 0;
 }
 
@@ -247,7 +67,6 @@ string decToHex(int x){
         
         int comp = div % 16;
         
-        
         if (comp > 9){
             //convert to letter
             hex = static_cast<char>(comp + 55) + hex;
@@ -255,7 +74,7 @@ string decToHex(int x){
         } else {
             hex = to_string(comp) + hex;
         }
-        
+
         
         div/= 16;
         
